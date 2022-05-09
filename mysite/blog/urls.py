62288f086263f9117d2814c2b41aa7db3f9e56d5
foxnorth228 <khitrii03@gmail.com>
2022-05-09 20:39:14 +0300

    Add more html and css files for pagination

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.mainPage, name='main_page'),
+
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,8 +4,60 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout, login
 from django.utils import timezone
+from .forms import SignupForm
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 import json
+#import logging
+#import configparser
 
+
+#config = configparser.ConfigParser()
+#config.read('cnf.ini')
+#logging.basicConfig(
+#    level=config['LOGGING']['level'],
+#    filename=config['LOGGING']['filename']
+#)
+#log = logging.getLogger(__name__)
 def mainPage(request):
     return render(request, 'blog/base.html')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+#            p = Process(target=log.info(f'{request.user} registred'))
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm(request.POST)
+    return render(
+        request,
+        'register/registration.html',
+        {'form': form}
+    )
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('post_list')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        print("form: ", form.data)
+        print('post: ', request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            if user is not None:
+                login(request, user)
+                return redirect('post_list')
+    else:
+        form = AuthenticationForm()
+    return render(
+        request,
+        'registration/login.html',
+        {'form': form}
+    )
+
+@login_required(login_url='login')
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
