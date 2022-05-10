2e412dde55b111c209efe5ac99c02a418012653e
foxnorth228 <khitrii03@gmail.com>
2022-05-10 15:04:09 +0300

    Upgrade auth and add simple pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout, login
 from django.utils import timezone
-from .forms import SignupForm
+from .forms import SignupForm, AuthForm
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 import json
 #import logging
@@ -19,7 +19,7 @@ import json
 #)
 #log = logging.getLogger(__name__)
 def mainPage(request):
-    return render(request, 'blog/base.html')
+    return render(request, 'blog/base.html', {'path': f"http://{request.get_host()}/blog"})
 
 def register_user(request):
     if request.method == 'POST':
@@ -33,14 +33,17 @@ def register_user(request):
     return render(
         request,
         'register/registration.html',
-        {'form': form}
+        {
+            'form': form,
+            'path': f"http://{request.get_host()}/blog",
+        }
     )
 
 def login_user(request):
     if request.user.is_authenticated:
         return redirect('post_list')
     if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
+        form = AuthForm(request, request.POST)
         print("form: ", form.data)
         print('post: ', request.POST)
         if form.is_valid():
@@ -49,11 +52,14 @@ def login_user(request):
                 login(request, user)
                 return redirect('post_list')
     else:
-        form = AuthenticationForm()
+        form = AuthForm()
     return render(
         request,
-        'registration/login.html',
-        {'form': form}
+        'register/login.html',
+        {
+            'form': form,
+            'path': f"http://{request.get_host()}/blog",
+        }
     )
 
 @login_required(login_url='login')
